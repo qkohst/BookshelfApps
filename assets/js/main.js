82025344b97773121ba23d3351103a43646aa4ca
qkohst <7311626-qkohst@users.noreply.gitlab.com>
2022-01-17 12:52:00 +0700

    Add Form edit

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+function showForm(a) {
+  document.getElementById("formEdit" + a).style.display = "block";
+}
+
+function hideForm(a) {
+  document.getElementById("formEdit" + a).style.display = "none";
+}
